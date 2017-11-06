42a639d25c5553e5030795fd3d3d2574e36ecd43
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-11-06 17:55:49 -0200

    chore: update README.md and GIM defaul url
    Updated defaul URL for GIM because the old one will stop working
    soon. The new url is https://gim.stone.com.br/api. The package
    version was also updated and the readme was update in order to
    show the getUser method of the management class and the way of
    instantiating the library.
    
    Resolves: #5

--- src/config/global-identity-config.ts
+++ src/config/global-identity-config.ts
@@ -2,12 +2,12 @@ import { getConfig } from './index'
 
 const gimConfiguration = getConfig({
   test: {
-    baseURL: 'https://arpexid.stone.com.br/api',
+    baseURL: 'https://gim.stone.com.br/api',
     applicationKey: process.env.GLOBAL_APP_KEY || 'test_aplication_key',
     apiKey: process.env.GLOBAL_API_KEY || 'test_api_key'
   },
   production: {
-    baseURL: process.env.GLOBAL_IDENTITY_URL || 'https://arpexid.stone.com.br/api',
+    baseURL: process.env.GLOBAL_IDENTITY_URL || 'https://gim.stone.com.br/api',
     applicationKey: process.env.GLOBAL_APP_KEY,
     apiKey: process.env.GLOBAL_API_KEY
   }
